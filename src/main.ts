31123e56d42c1b220cc151e4972033a93a984545
Nicolas Lépinay <nicolas@nosoft.io>
2024-11-22 16:42:01 +0100

    ADD Ecoute sur toutes les interfaces

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors();
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
